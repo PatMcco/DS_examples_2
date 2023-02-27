2f96b655d4fad401f54c0046fd97c65fb488ec12
Pat McCormick <90644414+PatMcco@users.noreply.github.com>
2023-02-27 14:03:06 -0400

    binary search tree and recursive methods added

--- list_folders/list_folders.cpp
+++ list_folders/list_folders.cpp
@@ -4,6 +4,15 @@
 
 namespace fs = std::filesystem;
 
+void list_folders_recursive(const fs::path& path, int indent=0) {
+    for (const auto& entry: fs::directory_iterator(path)) {
+        if (entry.is_directory()) {
+            std::cout << std::string(indent, ' ') << entry << std::endl;
+            list_folders_recursive(entry, indent + 2);
+        }
+    }
+}
+
 void list_folders(const fs::path& path) {
 
     std::queue<std::pair<fs::path, int>> paths;
@@ -14,7 +23,7 @@ void list_folders(const fs::path& path) {
         }
     }
     while (!paths.empty()) {
-        // pop one folder off the quque and go into that folder
+        // pop one folder off the queue and go into that folder
         auto current = paths.front();
         std::cout << std::string(current.second, ' ') << current.first << std::endl;
         paths.pop();
@@ -37,6 +46,13 @@ int main() {
     std::cout << std::endl;
 
     list_folders(path);
+
+    std::cout << std::endl;
+    std::cout << "Recursive..." << std::endl;
+    std::cout << "------------------" << std::endl;
+    std::cout << std::endl;
+
+    list_folders_recursive(path);
 
     return 0;
 }
